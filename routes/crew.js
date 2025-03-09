82f658e06afcd31095dd1268c42109bac800b8af
Liudmyla <perehrestliuda@gmail.com>
2025-03-09 23:33:35 +0200

    powerfull commit
    Co-Authored-By: blarneyyy <159807402+blarneyyy@users.noreply.github.com>
    Co-Authored-By: Artem <71194743+flex7r@users.noreply.github.com>

--- routes/crew.js
+++ routes/crew.js
@@ -4,10 +4,10 @@ var router = express.Router();
 const crew = {
     title: "Бриґада 1000-9 🎀",
     members: [
-        { name: "Руссу Артем", github: "https://github.com/flex7r", image: "flex7r.png", roles: ["розробник", "вайперр"] },
-        { name: "Перехрест Людмила", github: "https://github.com/brieflyl", image: "brieflyl.png", roles: ["тестувальник", "любить котів"] },
+        { id:"flex7r", name: "Руссу Артем", github: "https://github.com/flex7r", image: "flex7r.png", roles: ["розробник", "вайперр"] },
+        { id:"brieflyl", name: "Перехрест Людмила", github: "https://github.com/brieflyl", image: "brieflyl.png", roles: ["тестувальник", "любить котів"] },
         {
-            name: "Шнайдер Дарина", github: "https://github.com/blarneyyy", image: "blarneyy.png", roles: [
+            id:"blarneyyy", name: "Шнайдер Дарина", github: "https://github.com/blarneyyy", image: "blarneyy.png", roles: [
                 "пм", "мейн муерти", "керівник проєкту", "копірайтер", "менеджер проєкту",
                 "технічний лідер", "лідер команди", "клієнт", "аналітик/бізнес аналітик/експерт",
                 "системний адміністратор", "менеджер змін", "головний архітектор", "тестувальник",
@@ -20,6 +20,17 @@ const crew = {
 
 router.get('/', function (req, res, next) {
     res.render('crew', crew);
+});
+
+router.get('/:id', function (req, res, next) {
+    const memberId = req.params.id;
+    const member = crew.members.find(m => m.id === memberId);
+
+    if (!member) {
+        return res.status(404).send('Учасника не знайдено');
+    }
+
+    res.render('member', { title: member.id, member });
 });
 
 module.exports = router;
