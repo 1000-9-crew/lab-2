317ef458a1c99f25c815e76e1e0f6fa576a5776f
flex7r <artemrussu31@gmail.com>
2025-03-09 22:34:03 +0200

    dynamic, static pages
    Co-Authored-By: blarneyyy <159807402+blarneyyy@users.noreply.github.com>
    Co-Authored-By: Liudmyla <93478680+brieflyl@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var crewRouter = require('./routes/crew');
 
 var app = express();
 
@@ -17,8 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/crew', crewRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
